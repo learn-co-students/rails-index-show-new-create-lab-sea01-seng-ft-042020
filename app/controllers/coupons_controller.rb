f0f9ac0695d12b97b4d440950ebbd5bd464d9386
sap2411 <steven.parsons.us@gmail.com>
2020-05-15 11:05:58 -0700

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,5 @@
 class CouponsController < ApplicationController
-    before_action :set_coupon, only: [:show, :edit, :delete]
+    before_action :set_coupon, only: :show
     def index
         @coupons = Coupon.all
     end
@@ -14,7 +14,6 @@ class CouponsController < ApplicationController
         @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
         redirect_to @coupon
     end
-
 
     private
 
